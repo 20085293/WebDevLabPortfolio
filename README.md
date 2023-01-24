5daa02277dcce78f14ebab427e46799882474454
Glitch (sn-lab01-webapp-2023) <none>
2023-01-24 11:39:54 +0000

    🐅🦛 Updated with Glitch

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Hi
+# Lab #1 Playlist Website
+
+* Created Welcome page for playlist app using Semantic UI.
+* Added background colour in style.css
+* Added Javascript functionality to three buttons
---
+++ script.js
@@ -0,0 +1,38 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+console.log("Hello from the Web App Dev 1 lab");
+
+const greenbtn = document.querySelector(".green");
+
+greenbtn.addEventListener("click", () => alert("Thanks! You're okay too"));
+
+const bluebtn = document.querySelector(".blue");
+
+bluebtn.addEventListener("click", () => {
+    let readMoreDiv = document.querySelector("#readmore");
+    if (readMoreDiv.style.display === "block") 
+    {
+      readMoreDiv.style.display = "none";
+    } 
+  
+  else 
+  {
+      readMoreDiv.style.display = "block";
+    }
+});
+
+const redbtn = document.querySelector(".red");
+
+redbtn.addEventListener("click", () => {
+  let username = prompt("What's your name?");
+  let welcomeUserDiv = document.querySelector("#welcomeuser");
+  welcomeUserDiv.style.display = "block";
+  document.querySelector("#welcomeuser").innerHTML 
+    = `<p> Hello, ${username}, looking forward to hearing your playlists!
+    Click this message to close it.</p>`;
+  welcomeUserDiv.style.cursor = "pointer";
+});
+
+document.getElementById('welcomeuser').addEventListener("click",() => {
+document.querySelector("#welcomeuser").style.display= 'none'});
